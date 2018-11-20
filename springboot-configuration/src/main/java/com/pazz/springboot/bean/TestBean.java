39fca3def381e4c1cadfb181b96ebdafea69ca90
pazzf <pazzfj@163.com>
2018-11-20 20:39:32 +0800

    update name configuration

---
+++ springboot-configuration/src/main/java/com/pazz/springboot/bean/TestBean.java
@@ -0,0 +1,19 @@
+package com.pazz.springboot.bean;
+
+/**
+ * @author: 彭坚
+ * @create: 2018/8/27 13:45
+ * @description:
+ */
+public class TestBean {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ springboot-configuration/src/main/java/com/pazz/springboot/filter/TestFilter.java
@@ -0,0 +1,39 @@
+package com.pazz.springboot.filter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+/**
+ * @author: 彭坚
+ * @create: 2018/11/20 14:55
+ * @description:
+ */
+public class TestFilter implements Filter {
+
+    private static ServletContext servletContext;
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("Filter init...");
+        getServletContext(filterConfig);
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("Filter doFilter...");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    public void destroy() {
+        System.out.println("Filter destroy...");
+    }
+
+    public static void getServletContext(FilterConfig config) {
+        servletContext = config.getServletContext();
+    }
+
+}
