1b649c86442ce2ff2a695a347fba9c3339c07406
pazzf <pazzfj@163.com>
2018-12-03 00:37:23 +0800

    rocketmq-add

---
+++ springboot-rocketmq/src/main/java/com/pazz/springboot/rocketmq/exception/RocketMQException.java
@@ -0,0 +1,18 @@
+package com.pazz.springboot.rocketmq.exception;
+
+/**
+ * @author: 彭坚
+ * @create: 2018/12/3 0:33
+ * @description:
+ */
+public class RocketMQException extends RuntimeException {
+
+    public RocketMQException(String msg) {
+        super(msg);
+    }
+
+    public RocketMQException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+
+}
