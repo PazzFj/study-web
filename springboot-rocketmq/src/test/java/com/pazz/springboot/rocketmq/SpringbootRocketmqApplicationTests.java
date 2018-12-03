5b5cdd633f324022c2ebcfb1562f419b87396020
pazzf <pazzfj@163.com>
2018-12-03 23:26:08 +0800

    test

--- springboot-rocketmq/src/test/java/com/pazz/springboot/rocketmq/SpringbootRocketmqApplicationTests.java
+++ springboot-rocketmq/src/test/java/com/pazz/springboot/rocketmq/SpringbootRocketmqApplicationTests.java
@@ -1,16 +1,32 @@
 package com.pazz.springboot.rocketmq;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class SpringbootRocketmqApplicationTests {
 
-    @Test
-    public void contextLoads() {
+    public static void main(String[] args) {
+        try {
+            DefaultMQProducer producer = new DefaultMQProducer("please_rename_unique_group_name");
+            producer.setNamesrvAddr("47.98.219.97:9876");
+            producer.setVipChannelEnabled(false);
+            producer.start();
+            for (int i = 1000; i < 2000; i++) {
+                try {
+                    Message msg = new Message("TopicTest", "TagA", ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET));
+                    SendResult sendResult = producer.send(msg);
+                    System.out.printf("%s%n", sendResult);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Thread.sleep(1000);
+                }
+            }
+            producer.shutdown();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
