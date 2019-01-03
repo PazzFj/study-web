21e03e885dd152694124675b501e5689b5893a5e
pengjian <pengjian@eshippinggateway.com>
2019-01-03 17:45:31 +0800

    mybatis test

--- springboot-mybatis/src/test/java/com/pazz/springboot/mybatis/SpringbootMybatisApplicationTests.java
+++ springboot-mybatis/src/test/java/com/pazz/springboot/mybatis/SpringbootMybatisApplicationTests.java
@@ -1,5 +1,8 @@
 package com.pazz.springboot.mybatis;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,23 +14,31 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.XmlWebApplicationContext;
 
+import java.io.InputStream;
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootMybatisApplicationTests {
 
     @Test
     public void contextLoads() {
-        //spring-web 从Web应用下的一个或多个XML配置文件中加载应用上下文
-        ApplicationContext ac1 = new XmlWebApplicationContext();
-        //spring-context 从类路径下的一个或多个XML配置文件中加载上下文定义，把应用上下文的定义文件作为类资源
-        ApplicationContext ac2 = new ClassPathXmlApplicationContext();
-        //spring-context 从文件系统下的一个或多个XML配置中加载上下文定义
-        ApplicationContext ac3 = new FileSystemXmlApplicationContext();
-        //spring-context  从一个或多个基于Java的配置类中加载Spring应用上下文
-        ApplicationContext ac4 = new AnnotationConfigApplicationContext();
-        //spring-web 从一个或多个基于Java的配置类中加载Spring Web应用上下文
-        ApplicationContext ac5 = new AnnotationConfigWebApplicationContext();
+//        //spring-web 从Web应用下的一个或多个XML配置文件中加载应用上下文
+//        ApplicationContext ac1 = new XmlWebApplicationContext();
+//        //spring-context 从类路径下的一个或多个XML配置文件中加载上下文定义，把应用上下文的定义文件作为类资源
+//        ApplicationContext ac2 = new ClassPathXmlApplicationContext();
+//        //spring-context 从文件系统下的一个或多个XML配置中加载上下文定义
+//        ApplicationContext ac3 = new FileSystemXmlApplicationContext();
+//        //spring-context  从一个或多个基于Java的配置类中加载Spring应用上下文
+//        ApplicationContext ac4 = new AnnotationConfigApplicationContext();
+//        //spring-web 从一个或多个基于Java的配置类中加载Spring Web应用上下文
+//        ApplicationContext ac5 = new AnnotationConfigWebApplicationContext();
 
+        InputStream inputStream = SpringbootMybatisApplicationTests.class.getClassLoader().getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sessionFactory.openSession();
+        List<com.pazz.springboot.mybatis.entity.Test> tests = sqlSession.selectList("queryTestList");
+        System.out.println(tests.size());
     }
 
 }
