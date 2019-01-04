f1703ef81ee431d398cb29502ddb8da589c87feb
pazzf <pazzfj@163.com>
2019-01-04 20:16:56 +0800

    test interceptor

---
+++ springboot-mybatis/src/main/java/com/pazz/springboot/mybatis/interceptor/TestInterceptor.java
@@ -0,0 +1,46 @@
+package com.pazz.springboot.mybatis.interceptor;
+
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Intercepts;
+import org.apache.ibatis.plugin.Invocation;
+import org.apache.ibatis.plugin.Signature;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+
+import java.util.Properties;
+
+/**
+ * @author: 彭坚
+ * @create: 2019/1/3 20:56
+ * @description:
+ */
+@Intercepts({@Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class})})
+public class TestInterceptor implements Interceptor {
+
+    private int testProp;
+
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+        return null;
+    }
+
+    @Override
+    public Object plugin(Object target) {
+        return null;
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+
+    }
+
+    public int getTestProp() {
+        return testProp;
+    }
+
+    public void setTestProp(int testProp) {
+        this.testProp = testProp;
+    }
+}
