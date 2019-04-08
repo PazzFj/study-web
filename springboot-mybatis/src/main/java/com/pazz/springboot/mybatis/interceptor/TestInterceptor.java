222f1b933ce7444709af81107b0c724e236ffc33
pazzf <pazzfj@163.com>
2019-04-08 21:46:52 +0800

    update mybatis Plugin  Interceptor

--- springboot-mybatis/src/main/java/com/pazz/springboot/mybatis/interceptor/TestInterceptor.java
+++ springboot-mybatis/src/main/java/com/pazz/springboot/mybatis/interceptor/TestInterceptor.java
@@ -1,16 +1,16 @@
 package com.pazz.springboot.mybatis.interceptor;
 
+import com.pazz.springboot.mybatis.entity.LoginEntity;
 import org.apache.ibatis.cache.CacheKey;
 import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
-import org.apache.ibatis.plugin.Interceptor;
-import org.apache.ibatis.plugin.Intercepts;
-import org.apache.ibatis.plugin.Invocation;
-import org.apache.ibatis.plugin.Signature;
+import org.apache.ibatis.plugin.*;
 import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.RowBounds;
+import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -22,16 +22,37 @@ import java.util.Properties;
     @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class}),
     @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class, CacheKey.class, BoundSql.class})
 })
+@Component
 public class TestInterceptor implements Interceptor {
 
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
-        return null;
+        Object[] args = invocation.getArgs();
+        MappedStatement statements = (MappedStatement) args[0];
+        Object parameter = args[1];
+        RowBounds rowBounds = (RowBounds) args[2];
+        ResultHandler resultHandler = (ResultHandler) args[3];
+        Executor executor = (Executor) invocation.getTarget();
+        CacheKey cacheKey;
+        BoundSql boundSql;
+        if(args.length == 4){
+            //4 个参数时
+            boundSql = statements.getBoundSql(parameter);
+            cacheKey = executor.createCacheKey(statements, parameter, rowBounds, boundSql);
+        }else {
+            //6 个参数时
+            cacheKey = (CacheKey) args[4];
+            boundSql = (BoundSql) args[5];
+        }
+        BoundSql newBoundSql = new BoundSql(statements.getConfiguration(), boundSql.getSql() + "limit 0, 6", boundSql.getParameterMappings(), parameter);
+        System.out.println(newBoundSql.getSql());
+        List<LoginEntity> entityList = executor.query(statements, parameter, rowBounds, resultHandler, cacheKey, newBoundSql);
+        return entityList;
     }
 
     @Override
     public Object plugin(Object target) {
-        return null;
+        return Plugin.wrap(target, this);
     }
 
     @Override
