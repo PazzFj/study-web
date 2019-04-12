6dc09d643cd1bc09bb626e2ddb5d19a038a264b4
pengjian <pengjian@eshippinggateway.com>
2019-04-12 09:53:13 +0800

    v6 update configuration

--- springboot-redis-example/src/main/java/com/pazz/springboot/redis/example/configuration/CacheAutoConfiguration.java
+++ springboot-redis-example/src/main/java/com/pazz/springboot/redis/example/configuration/CacheAutoConfiguration.java
@@ -3,7 +3,8 @@ package com.pazz.springboot.redis.example.configuration;
 import com.pazz.springboot.redis.example.cache.redis.RedisCacheStorage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,7 +23,8 @@ public class CacheAutoConfiguration {
     private CacheProperties properties;
 
     @Bean
-//    @ConditionalOnBean({RedisTemplate.class})
+    @ConditionalOnClass(RedisTemplate.class)
+    @ConditionalOnMissingBean
     public RedisCacheStorage redisCacheStorage(RedisTemplate redisTemplate) {
         RedisCacheStorage redisCacheStorage = new RedisCacheStorage();
         redisCacheStorage.setRedisTemplate(redisTemplate);
--- springboot-redis-example/src/main/java/com/pazz/springboot/redis/example/configuration/RedisAutoConfiguration.java
+++ springboot-redis-example/src/main/java/com/pazz/springboot/redis/example/configuration/RedisAutoConfiguration.java
@@ -3,6 +3,7 @@ package com.pazz.springboot.redis.example.configuration;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -19,7 +20,8 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisAutoConfiguration {
 
     @Bean
-    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+    @ConditionalOnMissingBean
+    public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate redisTemplate = new RedisTemplate();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
 
