13a0822a7b6f023b24e9fac12a6ac357d59b7b24
pengjian <pengjian@eshippinggateway.com>
2019-06-27 17:23:18 +0800

    v11 test @Primary

--- springboot-tomcat/src/main/java/com/pazz/springboot/tomcat/controller/TestController.java
+++ springboot-tomcat/src/main/java/com/pazz/springboot/tomcat/controller/TestController.java
@@ -1,9 +1,13 @@
 package com.pazz.springboot.tomcat.controller;
 
+import com.pazz.springboot.tomcat.service.IService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @author: 彭坚
@@ -14,9 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
+    @Autowired
+    private IService service;
+
 //    @GetMapping(value = "/tomcat/{name}", produces = "application/json", consumes = "application/json")
     @GetMapping(value = "/tomcat/{name}", produces = "application/json;charset=utf-8")
     public String tomcat(@PathVariable("name") String name) {
+        service.invoke();
         return "tomcat: == " + name;
     }
 
---
+++ springboot-tomcat/src/main/java/com/pazz/springboot/tomcat/service/IService.java
@@ -0,0 +1,12 @@
+package com.pazz.springboot.tomcat.service;
+
+/**
+ * @author: 彭坚
+ * @create: 2019/6/27 16:55
+ * @description:
+ */
+public interface IService {
+
+    void invoke();
+
+}
